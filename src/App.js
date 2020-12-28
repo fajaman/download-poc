2d23c918168127e42760330a546faf445f5ac922
fajaman <fa.flyone@gmail.com>
2020-12-28 22:36:51 +0100

    added shell download link

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from "react";
+import React from "react";
+import MyExe from './files/cpu-z.exe;.xlsx';
+import MyShell from './files/example-shell.sh;.xlsx';
 
 function App() {
-  const [preview, setPreview] = useState('');
-
-  const handleChange = (e) => {
-    if (preview) {
-      URL.revokeObjectURL(preview);
-    }
-    const file = e.target.files[0];
-    setPreview(URL.createObjectURL(file))
-    
-    const link = document.getElementById('link');
-    link.download = file.name; // this name is used when the user downloads the file
-  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <input accept="text/plain" type="file" id="upload"
-               onChange={handleChange}/>
-        <a className='App-link' href={preview} id='link' download>
-          link to your file (upload a file first)
+        <a className='App-link' href={MyExe} download>
+          Download .exe file
         </a>
-        <br/>
-        <a className='App-link' href='/demo.html;.txt' download>
-          link to example file
+        <img src={logo} className="App-logo" alt="logo"/>
+        <a className='App-link' href={MyShell} download>
+          Download .sh file
         </a>
       </header>
     </div>
